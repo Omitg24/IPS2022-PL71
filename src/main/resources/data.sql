97802a530592842118bf2ca82c2edbad69182d47
Omitg24@ <Omitg@example.com>
2022-10-01 14:30:00 +0200

    01/10/2022 Import project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--Datos para carga inicial de la base de datos
+
+--Borrar todos los datos de las tablas primero
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+--Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
+--(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
+
+
+
