930c54f5d6274b551d47a532f05697eb84cac630
Alves <uo284288@uniovi.es>
2022-10-02 19:19:32 +0200

    02/10/22 - Alves - Añadidas tablas de la BBDD

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,48 @@
 --Primero se deben borrar todas las tablas (de detalle a maestro) y lugo anyadirlas (de maestro a detalle)
 --(en este caso en cada aplicacion se usa solo una tabla, por lo que no hace falta)
 
+DROP TABLE Solicitante;
+DROP TABLE Curso;
+DROP TABLE Solicitud;
 
+CREATE TABLE Solicitante (
+    DNIsol varchar(20) NOT NULL,
+    nombresol varchar(20) NOT NULL,
+    apellidossol varchar(40) NOT NULL,
+    localidadsol varchar(20) NOT NULL,
+    telefonosol varchar(20) NOT NULL,
+    titulacionsol varchar(40) NOT NULL,
+    centrosol varchar(40) NOT NULL,
+    anosol decimal(4, 0) NOT NULL,
+    IBANsol varchar(40) NOT NULL,
+    IDsol varchar(20) NOT NULL,
 
+    CONSTRAINT PK_SOLICITANTE PRIMARY KEY (DNIsol)
+);
+
+CREATE TABLE Curso (
+    titulocurso varchar(40) NOT NULL,
+    fechacurso date NOT NULL,
+    fechainicioins date NOT NULL,
+    fechafinins date NOT NULL,
+    precio decimal(10, 2) NOT NULL,
+    estadoc varchar(20) NOT NULL,
+    nplazas decimal(4, 0) NOT NULL,
+
+    CONSTRAINT PK_CURSO PRIMARY KEY (titulocurso),
+    CONSTRAINT CK_ESTADO_CURSO CHECK (estadoc in ('Planificado', 'Abierta', 'Cerrada', 'Cancelada')),
+    CONSTRAINT CK_FECHA_CURSO CHECK (fechainicioins < fechafinins)
+);
+
+CREATE TABLE Solicitud (
+    DNIsol varchar(20),
+    titulocurso varchar(40),
+    fecha date NOT NULL,
+    estados varchar(20) NOT NULL,
+    abonado decimal(10, 2) NOT NULL,
+
+    CONSTRAINT PK_SOLICITUD PRIMARY KEY (DNIsol, titulocurso),
+    CONSTRAINT FK_SOLICITUD_SOLICITANTE FOREIGN KEY (DNIsol) REFERENCES Solicitante (DNIsol),
+    CONSTRAINT FK_SOLICITUD_CURSO FOREIGN KEY (titulocurso) REFERENCES Curso (titulocurso),
+    CONSTRAINT CK_ESTADO_SOLICITUD CHECK (estados in ('Pre-inscrito', 'Inscrito', 'Cancelado'))
+);
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -2,3 +2,11 @@
 
 --Borrar todos los datos de las tablas primero
 
+delete from Solicitud;
+delete from Solicitante;
+delete from Curso;
+
+insert into Solicitante (dnisol, nombresol, apellidossol, localidadsol, telefonosol, titulacionsol, centrosol, anosol, ibansol, idsol) values
+	('7348920547A' ,'manolo', 'sanchez rodriguez', 'madrid', '472381032', 'panadero', 'uniovi', 1986, '3412431434789', '327894312'),
+	('6435634566K' ,'pablo', 'gonzalez martinez', 'asturias', '584022938', 'carnicero', 'fundacion minabo', 1983, '5342453352345', '465326453')
+;
